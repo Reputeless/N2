b2177de1ff3cf8423756c778cd3a6767fec42447
Ryo Suzuki <reputeless+github@gmail.com>
2024-08-13 00:48:46 +0900

    Update Main.cpp

--- lecture0/Main.cpp
+++ lecture0/Main.cpp
@@ -1,8 +1,73 @@
 ﻿#include <print>
 #include "MyLib/Common.hpp"
+//#include "MyLib/Utility.hpp"
+//#include "MyLib/Point.hpp"
+//#include "MyLib/FileSystem.hpp"
+//#include "MyLib/Timer.hpp"
+//#include "MyLib/BinaryFileWriter.hpp"
+//#include "MyLib/BinaryFileReader.hpp"
+//#include "MyLib/TextFileWriter.hpp"
+//#include "MyLib/TextFileReader.hpp"
+//#include "MyLib/Color.hpp"
+//#include "MyLib/Image.hpp"
+//#include "MyLib/BMP.hpp"
 
 int main()
 {
-	std::println("Hello, seccamp!");
-}
+	std::println("Hello, SecCamp!");
 
+	std::println("---- Common.hpp ----");
+	{
+
+	}
+
+	std::println("---- Utility.hpp ----");
+	{
+
+	}
+
+	std::println("---- Point.hpp ----");
+	{
+
+	}
+
+	std::println("---- FileSystem.hpp ----");
+	{
+
+	}
+
+	std::println("---- Timer.hpp ----");
+	{
+
+	}
+
+	std::println("---- BinaryFileWriter.hpp ----");
+	{
+
+	}
+
+	std::println("---- BinaryFileReader.hpp ----");
+	{
+
+	}
+
+	std::println("---- TextFileWriter.hpp ----");
+	{
+
+	}
+
+	std::println("---- TextFileReader.hpp ----");
+	{
+
+	}
+
+	std::println("---- Color.hpp ----");
+	{
+
+	}
+
+	std::println("---- Image.hpp & BMP.hpp ----");
+	{
+
+	}
+}
